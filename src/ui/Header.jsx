2bee8b1bc3b3d9c76b8e89927c0b353b714cf5ba
saadturky <saad.turky2000@gmail.com>
2025-04-21 18:52:27 +0300

    Add GitHub Pages deployment configuration

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <header className="bg-blue-600 dark:bg-gray-900 text-white dark:text-yellow-200 py-4 px-6 shadow-lg rounded-md transition-all duration-300">
       <h1 className="text-3xl font-bold flex items-center justify-center">
         <img
-          src="/taskmaster.png"
+          src="taskmaster.png"
           width="50"
           height="50"
           className="mr-2"
