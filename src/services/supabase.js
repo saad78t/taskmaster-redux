d3925eb7ea2af0d3a887936c064d2563e7ac7a21
saadturky <saad.turky2000@gmail.com>
2025-04-23 13:30:34 +0300

    ignore supabase

--- src/services/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://sbpcuubxhhdcznergrlo.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicGN1dWJ4aGhkY3puZXJncmxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzODEwNjEsImV4cCI6MjA1OTk1NzA2MX0.KC5obvJBazUjZTYdyRSMVekPmRIYkk9uXOzjj0b7EGk";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
